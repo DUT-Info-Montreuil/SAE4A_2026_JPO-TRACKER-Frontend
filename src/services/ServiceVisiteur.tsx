8f238e732e495e1b8185e8aba0b5bc8c7a7965bd
Antoine-Chen <antoinecoc2408@gmail.com>
2026-04-08 22:58:45 +0200

    add fetch pour modif visiteur dans le service

--- src/services/ServiceVisiteur.tsx
+++ src/services/ServiceVisiteur.tsx
@@ -87,4 +87,13 @@ export default class ServiceVisiteur {
             });
     }
 
+    static modifierVisiteur(id: string, data: any): Promise<any> {
+        return fetch(`${URL}/visiteurs/${id}`, {
+            method: "PUT",
+            body: JSON.stringify(data),
+            headers: {"Content-Type": "application/json"},
+        }).then(res => res.json())
+            .catch(error => console.error(error));
+    }
+
 }
